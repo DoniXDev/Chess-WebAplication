a9a54c74eb82175e58fca5d937ca9cf9035224ca
donix <tamas.donix@gmail.com>
2018-03-30 18:47:52 +0200

    Second
    Default web page HTML/CSS code added

--- index.php
+++ index.php
@@ -1,15 +1,43 @@
-<html>
+﻿<html>
 	<head>
 	  <title>Chess</title>
+	  <link rel="stylesheet" type="text/css" href="chess/style.css">
 	  <link rel="stylesheet" type="text/css" href="style.css">
 	  <meta HTTP-EQUIV="Content-Type" Content="text/html; Charset=iso-8859-2">
 	  <meta HTTP-EQUIV="Content-Language" Content="hu">
 	</head>
 
 	<body>
-		
-		<div class="circle"></div>
 	
+	<div id="menu"> 
+		<div class="menu-item"><p class="menu-item-p">Kezdőlap</p></div>
+		<div class="menu-item"><p class="menu-item-p">Újdonságok</p></div>
+		<div class="menu-item"><p class="menu-item-p">Ranglista</p></div>
+		<div class="menu-item"><p class="menu-item-p">Profil</p></div>
+		<div class="menu-item"><p class="menu-item-p">Bolt</p></div>
+	</div>
+	
+	<div id="login">
+		<form action="/action_page.php">
+			<label for="fname">Felhasználónév</label>
+			<input type="text" id="fname" name="firstname" placeholder="Írd be a felhasználóneved!">
+
+			<label for="lname">Jelszó</label>
+			<input type="text" id="lname" name="lastname" placeholder="Írd be a jelszavad!">
+		  
+			<input type="submit" value="Belépés">
+		</form>
+	</div>
+	
+	<div id="content">
 		
+		<div id="header">
+			<div id="title">Chess</div>
+		</div>
+		
+		
+	
+	</div>
+	
 	</body>
 </html>
