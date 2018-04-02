3857b631b5b634f029c7724f6855dd421fdd677a
donix <tamas.donix@gmail.com>
2018-04-02 13:10:23 +0200

    0.7
    + News page content added
    * fixed a bug with gold text
    * some changes

--- forms/form_logged.php
+++ forms/form_logged.php
@@ -9,16 +9,26 @@
 		else
 			$winrate = 0;
 		$lose = $data[0] - $data[1];
+		$gold = $data[3] . "G"
 		
 	?>
 	
-	<div class="loggeddiv"><h2> <?php echo $logged; ?> </h2></div>
+	<div class="loggeddiv">
+		<h2> <?php echo $logged; ?> </h2>
+	</div>
+	
 	<div style="border: 2px solid black;">
 		<p class="oli"> GY: <?php echo $data[1]; ?> | V: <?php echo $lose; ?> </p>
 		<p class="oli"> <?php echo $winrate; ?>% </p>
 	</div>
-	<div class="loggeddiv"><p class="oli"> Arany: <?php echo $data[3]; ?> </p></div>
-
+	
+	<div class="loggeddiv">
+		<p class="oli" style="color:gold; font-weight: bold;">
+			<?php echo $gold;?>
+		</p>
+	</div>
+	
+	<?php echo""; ?>
 	
 	<a href="login.php"><div class="menu-item" style="margin: 0% 10%; width:80%;"><p class="menu-item-p">Kilépés</p></div></a>
 </div>		
--- pages/news.php
+++ pages/news.php
@@ -1,1 +1,126 @@
 <h1>Újdonságok</h1>
+
+<div class="update">
+</div>
+
+<div class="update">
+	<h2>Weboldal Frissítés |0.1|</h2>
+
+		<h3>donixdev.esy.es/chess</h3>
+		
+		<h3>Hozzáadva</h3>
+		<ul>
+			<li>Főoldal</li>
+			<li>Hírek</li>
+			<li>Toplista</li>
+			<li>Bejelentkezés</li>
+			<li>Login API</li>
+			<li>Game API</li>
+			<li>Statics API</li>
+		</ul>
+	</p>
+</div>
+
+<div class="update">
+	<h2>Játék Frissítés |0.4.2|</h2>
+		<h3>Frissítve</h3>
+		<ul>
+			<li>Meccs adatok feltöltési hibája javítva</li>
+		</ul>		
+</div>
+
+<div class="update">
+	<h2>Játék Frissítés |0.4.1|</h2>
+		<h3>Hozzáadva</h3>
+		<ul>
+			<li>Meccs adatok feltöltése</li>
+		</ul>		
+</div>
+
+<div class="update">
+	<h2>Játék Frissítés |0.4|</h2>
+		<h3>Hozzáadva</h3>
+		<ul>
+			<li><b>Felhasználó regisztrálás és belépés</b></li>
+			<li>Meccskeresés rendszer</li>
+		</ul>
+		
+		<h3>Frissítve</h3>
+		<ul>
+			<li>Sakk szabályok</li>
+			<li>Felhasználónév valasztás</li>
+			<li>Belépés menü</li>
+			<li>Mecs létehozás</li>
+			<li>Álapotjelző sáv</li>
+		</ul>		
+</div>
+
+<div class="update">
+	<h2>Játék Frissítés |0.3.1|</h2>
+		<h3>Frissítve</h3>
+		<ul>
+			<li>Belejentkezési felület</li>
+			<li>Hibák javítása</li>
+		</ul>		
+</div>
+
+<div class="update">
+	<h2>Játék Frissítés |0.3|</h2>
+		<h3>Hozzáadva</h3>
+		<ul>
+			<li><b>Többjátékos mód: interneten keresztül</b></li>
+			<li>Meccskeresés rendszer</li>
+		</ul>
+		
+		<h3>Frissítve</h3>
+		<ul>
+			<li>Sakk szabályok</li>
+			<li>Felhasználónév valasztás</li>
+			<li>Belépés menü</li>
+			<li>Mecs létehozás</li>
+			<li>Álapotjelző sáv</li>
+		</ul>		
+</div>
+
+<div class="update">
+	<h2>Játék Frissítés |0.2.1|</h2>
+		<h3>Frissítve</h3>
+		<ul>
+			<li>Hibák javítása</li>
+		</ul>		
+</div>
+
+<div class="update">
+	<h2>Frissítés |0.2|</h2>
+		<h3>Hozzáadva</h3>
+		<ul>
+			<li><b>Tobjátékos mód: helyi 2 játékos</b></li>
+			<li>Játék gomb</li>
+			<li>Belépés menü</li>
+			<li>Felhasználónév valasztás</li>
+		</ul>
+		
+		<h3>Frissítve</h3>
+		<ul>
+			<li>Sakk szabályok</li>
+		</ul>		
+</div>
+
+<div class="update">
+	<h2>Frissítés |0.1|</h2>
+		<h3>Hozzáadva</h3>
+		<ul>
+			<li><b>Sakktábla rendszer</b></li>
+			<li>Sakk szabályok</li>
+			<li>Meccs létrehozás</li>
+			<li>Álapotjelző sáv</li>
+			<li>Kilépés gomb</li>
+			<li>Ikon</li>
+		</ul>
+		
+		<h3>Frissítve</h3>
+		<ul>
+			<li>Projekt név | <s>dChess</s> => Chess</li>
+			<li>Felület kinézet</li>
+		</ul>		
+</div>
