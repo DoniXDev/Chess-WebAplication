81582c91dcc94622dc0e3466b08fd9d4a3a23eb4
donix <tamas.donix@gmail.com>
2018-04-04 07:30:49 +0200

    0.10.1

--- pages/shop.php
+++ pages/shop.php
@@ -1,37 +1,11 @@
 <h1>Bolt</h1>
 
+ <?php
 
-
-<div class="shop-item">
-	<a href="./index.php?page=shop&buy="><div class="shop-item-buy">
-		<p class="shop-item-buy-p">Vásárlás</p>
-	</a></div>
-
-	<div class="shop-item-images">
-		<img class="shop-item-image" src="./units/BlackB.png">
-	</div>
-	
-	<div class="shop-item-images">
-		<img class="shop-item-image" src="./units/WhiteB.png">
-	</div>
-	
-	<div class="shop-item-images">
-		<b>50 arany</b>
-	</div>
-	
-	<div class="shop-item-images">
-		<b>Fehér változat</b>
-	</div>
-	
-	<div class="shop-item-images">
-		<b>Fekete változat</b>
-	</div>
-	
-	<div class="shop-item-images" >
-		<b>Ára</b>
-	</div>
-</div>
-<br>
+	$i=1;
+	while ($i<=10)
+	{
+?>
 
 <div class="shop-item">
 	<a href="./index.php?page=shop&buy="><div class="shop-item-buy">
@@ -63,153 +37,10 @@
 	</div>
 </div>
 <br>
-<div class="shop-item">
-	<a href="./index.php?page=shop&buy="><div class="shop-item-buy">
-		<p class="shop-item-buy-p">Vásárlás</p>
-	</a></div>
 
-	<div class="shop-item-images">
-		<img class="shop-item-image" src="./units/BlackB.png">
-	</div>
-	
-	<div class="shop-item-images">
-		<img class="shop-item-image" src="./units/WhiteB.png">
-	</div>
-	
-	<div class="shop-item-images">
-		<b>50 arany</b>
-	</div>
-	
-	<div class="shop-item-images">
-		<b>Fehér változat</b>
-	</div>
-	
-	<div class="shop-item-images">
-		<b>Fekete változat</b>
-	</div>
-	
-	<div class="shop-item-images" >
-		<b>Ára</b>
-	</div>
-</div>
-<br>
-<div class="shop-item">
-	<a href="./index.php?page=shop&buy="><div class="shop-item-buy">
-		<p class="shop-item-buy-p">Vásárlás</p>
-	</a></div>
+<?php 
 
-	<div class="shop-item-images">
-		<img class="shop-item-image" src="./units/BlackB.png">
-	</div>
-	
-	<div class="shop-item-images">
-		<img class="shop-item-image" src="./units/WhiteB.png">
-	</div>
-	
-	<div class="shop-item-images">
-		<b>50 arany</b>
-	</div>
-	
-	<div class="shop-item-images">
-		<b>Fehér változat</b>
-	</div>
-	
-	<div class="shop-item-images">
-		<b>Fekete változat</b>
-	</div>
-	
-	<div class="shop-item-images" >
-		<b>Ára</b>
-	</div>
-</div>
-<br>
-<div class="shop-item">
-	<a href="./index.php?page=shop&buy="><div class="shop-item-buy">
-		<p class="shop-item-buy-p">Vásárlás</p>
-	</a></div>
+	$i=$i+1;
+	}
 
-	<div class="shop-item-images">
-		<img class="shop-item-image" src="./units/BlackB.png">
-	</div>
-	
-	<div class="shop-item-images">
-		<img class="shop-item-image" src="./units/WhiteB.png">
-	</div>
-	
-	<div class="shop-item-images">
-		<b>50 arany</b>
-	</div>
-	
-	<div class="shop-item-images">
-		<b>Fehér változat</b>
-	</div>
-	
-	<div class="shop-item-images">
-		<b>Fekete változat</b>
-	</div>
-	
-	<div class="shop-item-images" >
-		<b>Ára</b>
-	</div>
-</div>
-<br>
-<div class="shop-item">
-	<a href="./index.php?page=shop&buy="><div class="shop-item-buy">
-		<p class="shop-item-buy-p">Vásárlás</p>
-	</a></div>
-
-	<div class="shop-item-images">
-		<img class="shop-item-image" src="./units/BlackB.png">
-	</div>
-	
-	<div class="shop-item-images">
-		<img class="shop-item-image" src="./units/WhiteB.png">
-	</div>
-	
-	<div class="shop-item-images">
-		<b>50 arany</b>
-	</div>
-	
-	<div class="shop-item-images">
-		<b>Fehér változat</b>
-	</div>
-	
-	<div class="shop-item-images">
-		<b>Fekete változat</b>
-	</div>
-	
-	<div class="shop-item-images" >
-		<b>Ára</b>
-	</div>
-</div>
-<br>
-<div class="shop-item">
-	<a href="./index.php?page=shop&buy="><div class="shop-item-buy">
-		<p class="shop-item-buy-p">Vásárlás</p>
-	</a></div>
-
-	<div class="shop-item-images">
-		<img class="shop-item-image" src="./units/BlackB.png">
-	</div>
-	
-	<div class="shop-item-images">
-		<img class="shop-item-image" src="./units/WhiteB.png">
-	</div>
-	
-	<div class="shop-item-images">
-		<b>50 arany</b>
-	</div>
-	
-	<div class="shop-item-images">
-		<b>Fehér változat</b>
-	</div>
-	
-	<div class="shop-item-images">
-		<b>Fekete változat</b>
-	</div>
-	
-	<div class="shop-item-images" >
-		<b>Ára</b>
-	</div>
-</div>
-<br>
+?>
