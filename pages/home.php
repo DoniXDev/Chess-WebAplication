320fd253a33e43aa6d5cb2175950076462199420
donix <tamas.donix@gmail.com>
2018-04-04 22:12:25 +0200

    0.12.2
    * image replaced

--- pages/home.php
+++ pages/home.php
@@ -1,19 +1,11 @@
 <!-- Home page, some information -->
 <h1>A játékról</h1>
-<p>
-Sakk
-Alapállás a sakktáblán
-Alapállás a sakktáblán
-Legfelsőbb vezető testület	Nemzetközi Sakkszövetség
-Eredet	Perzsia
-Leghíresebb sportolók	Híres sakkozók listája
-Kategória	táblajáték
-A sakk táblajáték két személy részére, és egyben sportág is. A „sakk” szó – amely nemcsak a játékot jelenti, hanem azt a helyzetet is, amikor az ellenfél királya „ütésben van” – a perzsa „shāh” (شَاه) szóból ered, amely uralkodót jelent. A sakk története a legendák világába nyúlik vissza.
-</p>
 
 <p>
 A sakkjátékot két játékos játssza egymás ellen a négyzet alakú, nyolc sorra és nyolc oszlopra felosztott sakktáblán, 16–16, azaz összesen 32 bábuval. A két játékos bábui határozottan eltérő színűek. A színek elnevezése világos, illetve sötét. Köznapi értelemben véve a sötét játékos bábui fekete színűek, míg a világoséi általában fehérek vagy világosbarnák (utóbbi főleg akkor, ha a bábukat fából készítették). A játékosok felváltva lépnek, és mindkettejük célja, hogy a másik fél király nevű figuráját a játékszabályok szerint bemattolják (azaz megtámadják, mégpedig úgy, hogy a támadást ne tudja elhárítani).
 </p>
+<img class="content-image" src="1.png" />
+<br>
 
 <h1>Játssz most!</h1>
 <p>
@@ -21,6 +13,5 @@ A program lehetőséget ad megméretteni magad a sakk világába, nincs más dol
 </p>
 <h3><a href="https://github.com/DoniXDev/Chess">Github</a>!</h3>
 
-<h2>A képre kattintva letöltheted a játékot.</h2>
+<h2>A játékot letölthetheted itt: <a href="http://donixdev.esy.es/Chess%200.4.1.zip">LETÖLTÉS</a>.</h2>
 
-<a href="http://donixdev.esy.es/Chess%200.4.1.zip"><img class="content-image" src="1.png" /></a>
