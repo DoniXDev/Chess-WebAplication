13ffea944726f9afb71c0427d497bfdf01d0b94d
donix <tamas.donix@gmail.com>
2018-04-04 21:42:41 +0200

    0.12
    + shop script added
    * shop function changes
    (Shop done)

--- pages/shop.php
+++ pages/shop.php
@@ -1,11 +1,16 @@
 <h1>Bolt</h1>
 
  <?php
+	
+	if ($_SERVER["REQUEST_METHOD"] == "GET")
+		if(!empty($_GET['buy']))
+			if(Buy($logged, $_GET['buy'][0], $_GET['buy'][1]))
+				echo "<h2>Sikeres vásárlás!</h2>";
+			else
+				echo "<h2>Hiba</h2>";
+	
 
-
- 
- 
- 
+	$pdata = GetPlayerDatas($logged);
 	$data = GetShopItems();
 	$max = count($data);
  
@@ -16,45 +21,45 @@
 ?>
 
 <div class="shop-item">
-
-	<a href="./index.php?page=shop&buy=<?php echo $split[0];?>:<?php echo $split[1];?>"><div class="shop-item-buy">
-		<p class="shop-item-buy-p">
-		<?php
-			echo "Vásárlás";
-		
-		?>
-		</p>
+	
+	<?php
+	if($pdata[4][$split[0]] != $split[1])
+	{
+	?>
+	
+	<a href="./index.php?page=shop&buy=<?php echo $split[0];?><?php echo $split[1];?>"><div class="shop-item-buy">
+		<p class="shop-item-buy-p"> Vásárlás</p>
 	</div></a>
 	
-	<div class="shop-item-images">
-		<img class="shop-item-image" src="./units/<?php echo $split[5];?>.png">
+	<?php
+	}
+	else
+	{
+	?>
+	
+	<div class="shop-item-selected">
+		<p class="shop-item-buy-p"> Kiválasztva</p>
 	</div>
 	
-	<div class="shop-item-images">
-		<img class="shop-item-image" src="./units/<?php echo $split[4];?>.png">
-	</div>
+	<?php
+	}
+	?>
 	
-	<div class="shop-item-images">
-		<b><?php echo $split[3];?> arany</b>
-	</div>
+	<div class="shop-item-images"><img class="shop-item-image" src="./units/<?php echo $split[5];?>.png"></div>
 	
-	<div class="shop-item-images">
-		<b>Fehér változat</b>
-	</div>
+	<div class="shop-item-images"><img class="shop-item-image" src="./units/<?php echo $split[4];?>.png"></div>
 	
-	<div class="shop-item-images">
-		<b>Fekete változat</b>
-	</div>
+	<div class="shop-item-images"><b><?php echo $split[3];?> arany</b></div>
 	
-	<div class="shop-item-images" >
-		<b><?php echo $split[2];?></b>
-	</div>
+	<div class="shop-item-images"><b>Fehér változat</b></div>
+	
+	<div class="shop-item-images"><b>Fekete változat</b></div>
+	
+	<div class="shop-item-images" ><b><?php echo $split[2];?></b></div>
 </div>
 <br>
 
 <?php 
-
 	$i=$i+1;
 	}
-
 ?>
