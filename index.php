181893f23e499186871c755dd27abd35e7965979
donix <tamas.donix@gmail.com>
2018-04-05 21:19:44 +0200

    0.12.4

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 <html>
 	<head>
 	  <title>Chess</title>
-	  <link rel="stylesheet" type="text/css" href="chess/style.css">
 	  <link rel="stylesheet" type="text/css" href="style.css">
 	  <meta HTTP-EQUIV="Content-Type" Content="text/html; Charset=iso-8859-2">
 	  <meta HTTP-EQUIV="Content-Language" Content="hu">
