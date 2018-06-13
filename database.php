360be7473208874132210ed37660a46cbedb96ec
donix <tamas.donix@gmail.com>
2018-06-13 13:51:18 +0200

    Delete database.php

--- database.php
+++
@@ -1,7 +0,0 @@
-<?php
-//new mysqli("mysql.hostinger.hu", "u467704677_root", "eyyo123", "u467704677_leake");
-//new mysqli("localhost", "root", "", "chess");
-$conn = new mysqli("mysql.hostinger.hu", "u467704677_root", "eyyo123", "u467704677_leake");
-if ($conn->connect_error)
-	die("Error");
-?>
